0943558673eb31b808dc36c6061d6b711134614b
Tim Bruecker <brooker_113@hotmail.com>
2018-03-23 11:20:06 -0700

    Points rendering

--- page/sketch.js
+++ page/sketch.js
@@ -1,5 +1,7 @@
 var table;
 var rows;
+var leftPoints = [];
+var rightPoints = [];
 
 
 function preload() 
@@ -9,13 +11,32 @@ table = loadTable('https://gist.githubusercontent.com/CanOSpam/27ff9648c32527a45
 
 function setup() 
 {
-var rows = table.getRows();
-
+	createCanvas(window.innerWidth, window.innerHeight);
+	var rows = table.getRows();
+	getPoints();
 	//warning: rows is an array of objects
-	for (var r = 0; r < rows.length; r++) 
+	console.log(leftPoints);
+	console.log(rightPoints);
+
+	strokeWeight(5);
+
+	leftPoints.forEach(function(el)
 	{
-		rows[r].arr.forEach(function(el) {console.log(el)});
-	}
+		stroke(255, 0, 0);
+		var u = parseFloat(el.u) + 100;
+		var v = parseFloat(el.v) + 100;
+		console.log("Left u: " + u + " v: " + v);
+		point(u, v);
+	})
+
+	rightPoints.forEach(function(el)
+	{
+		stroke(0, 0, 255);
+		var u = parseFloat(el.u) + 100;
+		var v = parseFloat(el.v) + 100;
+		console.log("Right u: " + u + " v: " + v);
+		point(u, v);
+	})
 
     
 }
@@ -26,7 +47,16 @@ function draw()
 
 }
 
-function getLeftPoints() 
+function getPoints() 
 {
+	var rows = table.getRows();
 
+	for (var r = 0; r < rows.length; r++) 
+	{
+		var pointL = {u:rows[r].arr[0], v:rows[r].arr[1]};
+		leftPoints.push(pointL);
+
+		var pointR = {u:rows[r].arr[4], v:rows[r].arr[5]};
+		rightPoints.push(pointR);
+	}
 }
